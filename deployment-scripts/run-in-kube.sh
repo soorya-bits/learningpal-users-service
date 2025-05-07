1f505e4e0a8ef45b55a88371a0be436fce4b0278
Sooryaraj Thangaraj <sooryaraj.thangaraj@doubleverify.com>
2025-05-07 13:34:04 +0530

    Kube scripts & Helm

---
+++ deployment-scripts/run-in-kube.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# === Configurable Variables ===
+SERVICE_NAME="users-service"
+IMAGE_NAME="librarypal-users-service"
+IMAGE_TAG="latest"
+HELM_CHART_DIR="./helm"
+LOCAL_PORT=8000
+SERVICE_PORT=8000
+
+# Exit on error
+set -e
+
+echo "[INFO] Using Minikube's Docker environment..."
+eval $(minikube docker-env)
+
+echo "[INFO] Building Docker image: ${IMAGE_NAME}:${IMAGE_TAG} ..."
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+
+echo "[INFO] Deploying Helm release: ${SERVICE_NAME} ..."
+helm upgrade --install ${SERVICE_NAME} ${HELM_CHART_DIR}
+
+echo "[INFO] Starting port-forward: localhost:${LOCAL_PORT} -> service/${IMAGE_NAME}:${SERVICE_PORT} ..."
+nohup kubectl port-forward service/${IMAGE_NAME} ${LOCAL_PORT}:${SERVICE_PORT} > port-forward.log 2>&1 &
+
+echo "[DONE] Service '${SERVICE_NAME}' is accessible at http://localhost:${LOCAL_PORT}/docs"
