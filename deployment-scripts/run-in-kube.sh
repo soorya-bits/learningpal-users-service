676df7d60c541564ecf97789d85c60bde84e3eb1
Sooryaraj Thangaraj <sooryaraj.thangaraj@doubleverify.com>
2025-05-07 13:55:50 +0530

    fix

--- deployment-scripts/run-in-kube.sh
+++ deployment-scripts/run-in-kube.sh
@@ -20,6 +20,10 @@ docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
 echo "[INFO] Deploying Helm release: ${SERVICE_NAME} ..."
 helm upgrade --install ${SERVICE_NAME} ${HELM_CHART_DIR}
 
+# Wait for the pod to be in 'Running' state
+echo "[INFO] Waiting for pod to be running..."
+kubectl wait --for=condition=available --timeout=600s deployment/${IMAGE_NAME}
+
 echo "[INFO] Starting port-forward: localhost:${LOCAL_PORT} -> service/${IMAGE_NAME}:${SERVICE_PORT} ..."
 nohup kubectl port-forward service/${IMAGE_NAME} ${LOCAL_PORT}:${SERVICE_PORT} > port-forward.log 2>&1 &
 
